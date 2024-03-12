12845e6a4f7818b3b5b6e41081cf4d9ed67f179e
Delphin Koumouna SAGNO <DSA21279@open-groupe.com>
2024-03-12 13:45:27 +0100

    unicafe 1.8: step 3 → Add Statistics component

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -2,6 +2,21 @@ import { useState } from 'react'
 
 const Button = ({ handleClickBtn, text }) => <button onClick={handleClickBtn}>{text}</button>
 const Display = ({ textToDisplay, value }) => <p><span>{textToDisplay}</span> <span>{value}</span></p>
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  return (
+    <>
+      <h1>statistics</h1>
+      <Display textToDisplay="Good: " value={good} />
+      <Display textToDisplay="Neutral: " value={neutral} />
+      <Display textToDisplay="Bad: " value={bad} />
+      <Display textToDisplay="All: " value={all} />
+      <Display textToDisplay="Average: " value={(good - bad) / all} />
+      <Display textToDisplay="Positive: " value={(good / all * 100) + ' %'} />
+    </>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -35,16 +50,10 @@ const App = () => {
   return (
     <div>
       <h1>give feedback, please!</h1>
-
       <Button handleClickBtn={goodFeedback} text="Good" />
       <Button handleClickBtn={neutralFeedback} text="Neutral" />
       <Button handleClickBtn={badFeedback} text="Bad" />
-      <h1>statistics</h1>
-      <Display textToDisplay="Good: " value={good} />
-      <Display textToDisplay="Neutral: " value={neutral} />
-      <Display textToDisplay="Bad: " value={bad} />
-      <Display textToDisplay="Average: " value={(good - bad) / (good + neutral + bad)} />
-      <Display textToDisplay="Positive: " value={(good / (good + neutral + bad) * 100) + ' %'} />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
